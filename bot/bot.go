a163c1468d44cddae5940778eaf9f6699159832b
Unanoc <leedaniil98@gmail.com>
2019-01-08 21:50:05 +0300

    func Run is implemented

---
+++ bot/bot.go
@@ -0,0 +1,42 @@
+package bot
+
+import (
+	"log"
+	"vkbot/database"
+
+	vkapi "github.com/dimonchik0036/vk-api"
+)
+
+// Run runs the bot.
+func Run(db *database.DB, accessToken string) {
+	bot, err := vkapi.NewClientFromToken(accessToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Log(true)
+
+	if err := bot.InitLongPoll(0, 2); err != nil {
+		log.Panic(err)
+	}
+
+	LPCfg := vkapi.LPConfig{
+		Wait: 25,
+		Mode: vkapi.LPModeAttachments,
+	}
+	updates, _, err := bot.GetLPUpdatesChan(100, LPCfg)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var msg *vkapi.LPMessage
+
+	for update := range updates {
+		msg = update.Message
+
+		if msg == nil || !update.IsNewMessage() || msg.Outbox() {
+			continue
+		}
+		go msgHandler(bot, db, msg.FromID, msg.Text)
+	}
+}
