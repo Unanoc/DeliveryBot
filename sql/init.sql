08d664b28be20a4eaa8d91b75b8446b211039c06
Unanoc <leedaniil98@gmail.com>
2019-01-08 21:48:39 +0300

    Updated schema

--- sql/init.sql
+++ sql/init.sql
@@ -3,7 +3,18 @@ CREATE TABLE IF NOT EXISTS users (
   "id"        BIGINT UNIQUE NOT NULL PRIMARY KEY,
   "firstname" CITEXT NOT NULL,
   "lastname"  CITEXT NOT NULL,
-  "gender"    CHAR[1],
+  "sex"       CHAR[1],
   "age"       INT
 );
 
+CREATE TABLE IF NOT EXISTS order (
+  "id"          SERIAL UNIQUE PRIMARY KEY,
+  "user_id"     BIGINT NOT NULL,
+  "firstname"   CITEXT DEFAULT NULL,
+  "lastname"    CITEXT DEFAULT NULL,
+  "phone"       CITEXT DEFAULT NULL,
+  "company"     CITEXT DEFAULT NULL,
+  "address"     CITEXT DEFAULT NULL,
+  "date"        TIMESTAMPTZ(3) DEFAULT NULL
+  "is_finished" BOOLEAN DEFAULT FALSE
+)
