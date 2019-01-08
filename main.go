69479cbaea50f9da78e9cbaf02eb9db3798f5405
Unanoc <leedaniil98@gmail.com>
2019-01-08 16:39:17 +0300

    main.go is written

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+	"vkbot/bot"
+	"vkbot/config"
+	"vkbot/database"
+)
+
+var pathToConfig = flag.String("config", "", "Path to configuration JSON file")
+
+func main() {
+	flag.Parse()
+
+	// Setting of configuration
+	configJSON, err := ioutil.ReadFile(*pathToConfig)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	config := config.Config{}
+	if err := json.Unmarshal(configJSON, &config); err != nil {
+		log.Panic(err)
+	}
+
+	// Connecting to database
+	db := database.DB{}
+	if err := db.Connect(config.Connection); err != nil {
+		log.Panic(err)
+	}
+	defer db.Disconnect()
+
+	// Launching bot
+	bot.Run(&db, config.AccessToken)
+}
