5509c7cb38d61067b14bad94f1425311b02c5208
Unanoc <leedaniil98@gmail.com>
2019-01-09 17:21:55 +0300

    improved sql queries

--- database/queries.go
+++ database/queries.go
@@ -1,57 +1,48 @@
 package database
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
-// CreateOrUpdateUserState sets the state of user in database.
+// CreateOrUpdateUserState changes the state of user in database.
 func CreateOrUpdateUserState(db *DB, userID int64, state int) {
-	if existsCurrentOrderByID(db, userID) {
-		_, err := db.Conn.Exec(`
-			UPDATE orders
-			SET "state" = $2
-			WHERE "user_id" = $1 AND NOT "is_finished"
-		`, &userID, &state)
-		if err != nil {
-			log.Println()
-			log.Println(err)
-			log.Println()
-		}
-	} else {
-		_, err := db.Conn.Exec(`
-			INSERT INTO orders
-			("user_id", "firstname", "lastname", "phone", "company", "address", "date", "state")
-			VALUES ($1, NULL, NULL, NULL, NULL, NULL, NULL, $2)
-		`, &userID, &state)
-		if err != nil {
-			log.Println()
-			log.Println(err)
-			log.Println()
-		}
+	if _, err := db.Conn.Exec(sqlInsertUserState, &userID, &state); err != nil {
+		db.Conn.Exec(sqlUpdateUserState, &userID, &state)
 	}
 }
 
 // GetUserStateByID selects the state of orders.
-func GetUserStateByID(db *DB, userID int64) int {
-	var state int
-	err := db.Conn.QueryRow(`
-		SELECT "state" 
-		FROM orders 
-		WHERE "user_id" = $1 AND NOT "is_finished"
-	`, &userID).Scan(&state)
-
-	if err != nil {
-		return 0 // StateNull
-	}
-
-	return state
+func GetUserStateByID(db *DB, userID int64) (state int) {
+	db.Conn.QueryRow(sqlSelectUserStateByUserID, &userID).Scan(&state)
+	return // if user's state is absent, return 0 by default
 }
 
-// SelectFinishedOrderByID selects record of completed order.
-func SelectFinishedOrderByID(db *DB, userID int64) (*Order, error) {
+// CreateOrder creates an order.
+func CreateOrder(db *DB, userID int64) {
+	orderDate := time.Now().Format("2006-01-02 15:04:05")
+	db.Conn.Exec(sqlInsertOrder, &userID, &orderDate)
+}
+
+// UpdateOrder updates order.
+func UpdateOrder(db *DB, userID int64, field, value string) {
+	sqlQuery := fmt.Sprintf("UPDATE orders SET %s = '%s' WHERE user_id = $1 AND order_date = (SELECT MAX(order_date) FROM orders)", field, value)
+
+	if _, err := db.Conn.Exec(sqlQuery, &userID); err != nil {
+		log.Println("1")
+		log.Println(err)
+		log.Println()
+	}
+}
+
+// SelectOrderByID selects an order by user_id from db.
+func SelectOrderByID(db *DB, userID int64) (*Order, error) {
 	var order Order
 	err := db.Conn.QueryRow(`
-		SELECT "id", "firstname", "lastname", "phone", "company", "address", "date"
+		SELECT "id", "firstname", "lastname", "phone", "company", "address", "delivery_date", "order_date"
 		FROM orders
-		WHERE "user_id" = $1 AND "is_finished" AND "date" = (SELECT MAX("date") FROM orders)
+		WHERE "user_id" = $1 AND "order_date" = (SELECT MAX("order_date") FROM orders)
 	`, &userID,
 	).Scan(
 		&order.OrderID,
@@ -60,42 +51,16 @@ func SelectFinishedOrderByID(db *DB, userID int64) (*Order, error) {
 		&order.Phone,
 		&order.Company,
 		&order.Address,
-		&order.Date,
+		&order.DeliveryDate,
+		&order.OrderDate,
 	)
 
 	if err != nil {
-		log.Println()
+		log.Println("2")
 		log.Println(err)
 		log.Println()
 		return nil, err
 	}
 
 	return &order, nil
-}
-
-// SetFinishFlagOrder sets field "is_finished" in order.
-func SetFinishFlagOrder(db *DB, userID int64) {
-	db.Conn.Exec(`
-		UPDATE orders
-		SET "is_finished" = true
-		WHERE "user_id" = $1 AND NOT "is_finished" AND "date" = (SELECT MAX("date") FROM orders)
-	`, &userID)
-}
-
-func existsCurrentOrderByID(db *DB, userID int64) bool {
-	var id int
-	err := db.Conn.QueryRow(`
-		SELECT "id"
-		FROM orders
-		WHERE "user_id" = $1 AND NOT "is_finished"
-	`, &userID).Scan(&id)
-
-	if err != nil {
-		log.Println()
-		log.Println(err)
-		log.Println()
-		return false
-	}
-
-	return true
 }
