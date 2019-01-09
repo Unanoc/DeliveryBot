65ed36b91c328c93248fcc52c915dbd51e489de4
Unanoc <leedaniil98@gmail.com>
2019-01-09 17:21:37 +0300

    changed an order structure

--- database/models.go
+++ database/models.go
@@ -7,25 +7,25 @@ import (
 
 // Order is a struct for order confirmation.
 type Order struct {
-	OrderID    int
-	FirstName  string
-	LastName   string
-	Phone      string
-	Company    string
-	Address    string
-	Date       time.Time
-	State      int
-	IsFinished bool
+	OrderID      int
+	FirstName    string
+	LastName     string
+	Phone        string
+	Company      string
+	Address      string
+	DeliveryDate string
+	OrderDate    time.Time
 }
 
 func (o Order) String() string {
-	return fmt.Sprintf("Номер заказа: %d\nИмя: %s\n, Фамилия: %s\n, Телефон: %s\n, Компания: %s\n, Адрес: %s\n, Дата: %s",
+	return fmt.Sprintf("Номер заказа: %d\nИмя: %s\nФамилия: %s\nТелефон: %s\nКомпания: %s\nАдрес: %s\nДата доставки: %s\nДата заказа: %s",
 		o.OrderID,
 		o.FirstName,
 		o.LastName,
 		o.Phone,
 		o.Company,
 		o.Address,
-		o.Date.Format("2006-01-02 15:04:05"),
+		o.DeliveryDate,
+		o.OrderDate.Format("2006-01-02 15:04:05"),
 	)
 }
