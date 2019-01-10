50cd3d5fb4b552458346f43372e8516ef844b6a6
Unanoc <leedaniil98@gmail.com>
2019-01-10 05:24:43 +0300

    updated deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 docker build --no-cache .. -t vk-bot
 docker rm vkbot -f
-docker run --name vkbot -d vk-bot ./vkbot -config="config.json"
+docker run --name vkbot -d vk-bot /etc/init.d/postgresql start && ./vkbot -config="config.json"
 docker ps
