0a2d5fae1d368cbd2dc934be6bc19dfa5e69dbfb
Unanoc <leedaniil98@gmail.com>
2019-01-14 15:35:46 +0300

    refactoring of sql.go

--- database/sql.go
+++ database/sql.go
@@ -1,12 +1,13 @@
 package database
 
-// CreateOrUpdateUser
-const sqlInsertUser = `
+const (
+	// CreateOrUpdateUser
+	sqlInsertUser = `
 	INSERT INTO users
 	("id", "firstname", "lastname", "age", "sex")
 	VALUES ($1, $2, $3, $4, $5)
 `
-const sqlUpdateUser = `
+	sqlUpdateUser = `
 	UPDATE users
 	SET "firstname" = $2, 
 		"lastname" = $3,
@@ -15,45 +16,46 @@ const sqlUpdateUser = `
 	WHERE "id" = $1
 `
 
-// CreateOrUpdateUserState
-const sqlInsertUserState = `
+	// CreateOrUpdateUserState
+	sqlInsertUserState = `
 	INSERT INTO states
 	("user_id", "state")
 	VALUES ($1, $2)
 `
-const sqlUpdateUserState = `
+	sqlUpdateUserState = `
 	UPDATE states
 	SET "state" = $2
 	WHERE "user_id" = $1
 `
 
-// GetUserStateByID
-const sqlSelectUserStateByUserID = `
+	// GetUserStateByID
+	sqlSelectUserStateByUserID = `
 	SELECT "state" 
 	FROM states 
 	WHERE "user_id" = $1
 `
 
-// CreateOrder
-const sqlInsertOrder = `
+	// CreateOrder
+	sqlInsertOrder = `
 	INSERT INTO orders
 	("user_id", "order_date")
 	VALUES ($1, $2)
 `
 
-// UpdateOrder
-var sqlUpdateOrder = "UPDATE orders SET %s = '%s' WHERE user_id = $1 AND order_date = (SELECT MAX(order_date) FROM orders WHERE user_id = $1)"
-
-// SelectOrderByID
-const sqlSelectOrderByID = `
+	// SelectOrderByID
+	sqlSelectOrderByID = `
 	SELECT "id", "firstname", "lastname", "phone", "company", "address", "delivery_date", "order_date"
 	FROM orders
 	WHERE "user_id" = $1 AND "order_date" = (SELECT MAX("order_date") FROM orders WHERE "user_id" = $1)
 `
 
-// DeleteOrder
-const sqlDeleteOrder = `
+	// DeleteOrder
+	sqlDeleteOrder = `
 	DELETE 
 	FROM orders 
 	WHERE "user_id" = $1 AND "order_date" = (SELECT MAX("order_date") FROM orders WHERE "user_id" = $1)
 `
+)
+
+// UpdateOrder
+var sqlUpdateOrder = "UPDATE orders SET %s = '%s' WHERE user_id = $1 AND order_date = (SELECT MAX(order_date) FROM orders WHERE user_id = $1)"
